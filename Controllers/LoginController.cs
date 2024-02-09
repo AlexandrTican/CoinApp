13a8f79a9033955c55534c78fba5cf17bab0b85c
Tican Alexandr <alexandr.tican@iis.utm.md>
2024-02-09 22:29:57 +0200

    feat: Implement user authentication and JWT token generation in LoginController

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using CoinAppAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace CoinAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        private Users AuthenticateUser(Users user)
+        {
+            Users _user = null;
+            if (user.Username == "admin" && user.Password == "55f14NBoYJRF1SK")
+            {
+                _user = new Users() { Username = "Ilya Zlobin" };
+            }   
+
+            return _user;
+        }
+
+        private string GenerateToken(Users users)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Audience"], null,
+                expires: DateTime.Now.AddMinutes(1),
+                signingCredentials: credentials
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        
+        
+    }
+        
+}
